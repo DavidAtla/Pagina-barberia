9521d76946f95851844d615eac1c081c99c1d185
AMV147 <davidatlmar@gmail.com>
2025-02-20 21:16:07 -0600

    SOlucion

---
+++ src/pages/NuestrosCursosPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="full-width">
+    <div class="q-pa-md fondo-imagen-1 container padding-por-header">
+      <h1 class="text-h6">Nuestros Cursos</h1>
+    </div>
+
+    <DetallesCursosComponent />
+  </q-page>
+</template>
+
+<script setup>
+import DetallesCursosComponent from 'src/components/DetallesCursosComponent.vue'
+
+//
+</script>
