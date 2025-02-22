7577d75395c0c7a1accdfd68e7cbc3015ad9d0fe
DavidAtla <davidatlmar@gmail.com>
2025-02-22 09:49:04 -0600

    Agregar informacion

--- src/components/NuestrosCursosComponent.vue
+++ src/components/NuestrosCursosComponent.vue
@@ -1,17 +1,40 @@
 <template>
   <div class="q-pa-md container bg-grey-1">
-    <div class="row">
+    <div class="col-md-6 col-12">
+      <!-- Primera -->
+      <q-page class="flex flex-center bg-grey-2">
+        <div class="row q-pa-xl items-center">
+          <!-- Imagen -->
+          <div class="col-xs-12 col-md-6">
+            <q-img src="src\assets\background-images\Home.png" fit="cover" />
+          </div>
+
+          <!-- Sección de Texto -->
+          <div class="col-xs-12 col-md-6">
+            <q-card flat class="q-pa-md text-center bg-grey-1">
+              <q-card-section>
+                <div class="text-h4 text-bold text-dark">GET YOUR STYLE YOU DESERVE!</div>
+                <q-separator class="q-mt-md q-mb-md" />
+                <q-icon name="mdi-mustache" size="md" color="brown" class="q-mb-md" />
+                <div class="text-body1 text-grey-8">
+                  BarberShop is a professional context. It often happens that private or clients
+                  order a publication to be made and presented with the actual content still not
+                  being ready. However, reviewers tend to be distracted by content, say, a random
+                  text copied from the internet.
+                </div>
+              </q-card-section>
+              <q-card-actions align="center">
+                <q-btn class="botonver" label="VER MÁS"></q-btn>
+              </q-card-actions>
+            </q-card>
+          </div>
+        </div>
+      </q-page>
+
+      <!--Primera -->
+      <!-- Final-->
       <div class="col-md-6 col-12">
         <div class="contenedor-curso">
-          <h2 class="text-h4">GET YOUR STYLE<br />YOU DESERVE!</h2>
-          <div class="borde-rojo q-pa-md">
-            <center>
-              Este esquema está diseñado para que aprendas desde cero y alcances la especialidad,
-              siendo capaz de potenciar tu trabajo.
-            </center>
-          </div>
-        </div>
-        <div class="contenedor-curso">
           <h2 class="text-h4">MASTER BARBER</h2>
           <div class="borde-rojo q-pa-md">
             <center>
@@ -20,14 +43,14 @@
             </center>
           </div>
         </div>
-        <div class="contenedor-curso">
-          <h2 class="text-h4">MASTER BARBER</h2>
-          <div class="borde-rojo q-pa-md">
-            <center>
-              Este esquema está diseñado para que aprendas desde cero y alcances la especialidad,
-              siendo capaz de potenciar tu trabajo.
-            </center>
-          </div>
+      </div>
+      <div class="contenedor-curso">
+        <h2 class="text-h4">MASTER BARBER</h2>
+        <div class="borde-rojo q-pa-md">
+          <center>
+            Este esquema está diseñado para que aprendas desde cero y alcances la especialidad,
+            siendo capaz de potenciar tu trabajo.
+          </center>
         </div>
       </div>
     </div>
@@ -47,5 +70,14 @@ export default {
 </script>
 
 <style scoped>
-/* Add any component-specific styles here */
+.q-img {
+  height: 560px;
+  width: 100%;
+  border-radius: 20px;
+}
+.q-card {
+  max-width: 920px;
+  height: 560px;
+  border-radius: 20px;
+}
 </style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -8,7 +8,7 @@
     <NuestrosCursosComponent />
 
     <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4495.876329342003!2d-98.202048401089!3d19.028077752942192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cfc1000eb3f76d%3A0x6fa14040b8a1bb1!2sAcademy%20House%20of%20Shaves%20Barbershop%20%26%20Raffaelo!5e0!3m2!1ses-419!2smx!4v1740153875393!5m2!1ses-419!2smx"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2667.0323507954367!2d-98.25723846299451!3d19.030585119290862!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cfc70da632cbc7%3A0x7de8f9bb4ce24a9f!2sPlaza%20San%20martinito%2C%20C.%20Mira%20213-Local%232%2C%20Reserva%20Territorial%20Atlixc%C3%A1yotl%2C%20Paseos%20de%20San%20Andr%C3%A9s%2C%2072810%20San%20Andr%C3%A9s%20Cholula%2C%20Pue.!5e0!3m2!1ses-419!2smx!4v1740171221991!5m2!1ses-419!2smx"
       width="100%"
       height="300"
       style="border: 0"
