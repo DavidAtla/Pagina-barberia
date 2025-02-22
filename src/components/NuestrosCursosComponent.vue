785c9dee4f04c2aea3b4221285174f1dee904ab6
DavidAtla <davidatlmar@gmail.com>
2025-02-22 13:55:54 -0600

    Servicios de Menu

--- src/components/NuestrosCursosComponent.vue
+++ src/components/NuestrosCursosComponent.vue
@@ -30,9 +30,87 @@
           </div>
         </div>
       </q-page>
-
-      <!--Primera -->
       <!-- Final-->
+      <!--Segunda-->
+      <q-page padding class="page-bg">
+        <!-- Título Principal -->
+        <div class="custom-title text-h6 text-center q-pa-md">SERVICIOS DE MENU</div>
+
+        <div class="row q-col-gutter-lg">
+          <!-- Menú 1 -->
+          <div class="col-12 col-md-4 q-mb-lg">
+            <q-card flat class="bg-transparent q-pa-md text-center border-light-brown">
+              <q-card-section>
+                <!-- Logo -->
+                <img src="src\assets\background-images\Menu1.png" alt="Logo" class="logo-image" />
+                <!-- Título -->
+                <div class="text-h6 q-mt-sm">Barba</div>
+                <!-- Descripción -->
+                <div class="text-body2 q-mt-sm text-white descripcion">
+                  Ritual de Barba: . . . . . . . . . . . . $300 MXN
+                  <div class="text-body2 q-mt-sm text-white">
+                    Disfruta de un corte tradicional con atención personalizada y un ambiente único.
+                  </div>
+                </div>
+                <div class="text-body2 q-mt-sm text-white descripcion">
+                  Barba sin Ritual: . . . . . . . . . . . . $200 MXN
+                  <br />
+                  Disfruta de un corte tradicional con atención personalizada y un ambiente único.
+                </div>
+                <div class="text-body2 q-mt-sm text-white descripcion">
+                  Disfruta de un corte tradicional con atención personalizada y un ambiente único.
+                </div>
+              </q-card-section>
+            </q-card>
+          </div>
+
+          <!-- Menú 2 -->
+          <div class="col-12 col-md-4 q-mb-lg">
+            <q-card flat class="bg-transparent q-pa-md text-center border-light-brown">
+              <q-card-section>
+                <!-- Logo -->
+                <img src="src\assets\background-images\Menu2.png" alt="Logo" class="logo-image" />
+                <!-- Título -->
+                <div class="text-h6 q-mt-sm">Cabello</div>
+                <!-- Descripción -->
+                <div class="text-body2 q-mt-sm text-white">
+                  Cortes modernos, afeitados y arreglos de barba para el hombre contemporáneo.
+                </div>
+              </q-card-section>
+            </q-card>
+          </div>
+
+          <!-- Menú 3 -->
+          <div class="col-12 col-md-4 q-mb-lg">
+            <q-card flat class="bg-transparent q-pa-md text-center border-light-brown">
+              <q-card-section>
+                <!-- Logo -->
+                <img src="src\assets\background-images\Menu3.png" alt="Logo" class="logo-image" />
+                <!-- Título -->
+                <div class="text-h6 q-mt-sm">Completo</div>
+                <!-- Descripción -->
+                <div class="text-body2 q-mt-sm text-white">
+                  Reserva tu cita o haznos cualquier consulta. Estamos para atenderte.
+                </div>
+              </q-card-section>
+              <!-- Para Agregar un boton -->
+              <!-- <q-card-actions align="center"> -->
+              <!-- <q-btn label="Contactar" color="primary" /> -->
+              <!-- </q-card-actions> -->
+            </q-card>
+          </div>
+        </div>
+      </q-page>
+      <!--Final-->
+      <div class="contenedor-curso">
+        <h2 class="text-h4">MASTER BARBER</h2>
+        <div class="borde-rojo q-pa-md">
+          <center>
+            Este esquema está diseñado para que aprendas desde cero y alcances la especialidad,
+            siendo capaz de potenciar tu trabajo.
+          </center>
+        </div>
+      </div>
       <div class="col-md-6 col-12">
         <div class="contenedor-curso">
           <h2 class="text-h4">MASTER BARBER</h2>
@@ -44,13 +122,15 @@
           </div>
         </div>
       </div>
-      <div class="contenedor-curso">
-        <h2 class="text-h4">MASTER BARBER</h2>
-        <div class="borde-rojo q-pa-md">
-          <center>
-            Este esquema está diseñado para que aprendas desde cero y alcances la especialidad,
-            siendo capaz de potenciar tu trabajo.
-          </center>
+      <div class="col-md-6 col-12">
+        <div class="contenedor-curso">
+          <h2 class="text-h4">MASTER BARBER</h2>
+          <div class="borde-rojo q-pa-md">
+            <center>
+              Este esquema está diseñado para que aprendas desde cero y alcances la especialidad,
+              siendo capaz de potenciar tu trabajo.
+            </center>
+          </div>
         </div>
       </div>
     </div>
@@ -68,16 +148,4 @@ export default {
   },
 }
 </script>
-
-<style scoped>
-.q-img {
-  height: 560px;
-  width: 100%;
-  border-radius: 20px;
-}
-.q-card {
-  max-width: 920px;
-  height: 560px;
-  border-radius: 20px;
-}
-</style>
+<style></style>
